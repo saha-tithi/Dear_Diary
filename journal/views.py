041614e0c4ad4454d9356bf679805f234cc2ae4e
saha-tithi <email.tithisaha@gmail.com>
2026-06-09 15:42:47 +0530

    working on models and view

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models  import journalEntry
+from django .contrib.auth.decorators import login_required
+ 
+@login_required
+def entry_list(request);
+    entries=journalEntry.objects.filter(user=request.user)
+    return render(request,'entry_list.html',{'entries': entries})
