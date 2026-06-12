5fb1c13546c129a32081b22b2170327c695eb268
saha-tithi <email.tithisaha@gmail.com>
2026-06-12 13:23:08 +0530

    added edit and deletr features

--- journal/urls.py
+++ journal/urls.py
@@ -3,5 +3,8 @@ from .import views
 
 urlpatterns=[path('', views.entry_list,name='entry_list'),
              path('new/', views.create_entry,name='create_entry'),
+             path('edit/<int:id>/', views.edit_entry,name='edit_entry'),
+             path('delete/<int:id>/', views.delete_entry,name='delete_entry'),
+
              
                ]
--- journal/views.py
+++ journal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models  import journalEntry
 from django .contrib.auth.decorators import login_required
  
@@ -15,4 +15,25 @@ def create_entry(request):
         journalEntry.objects.create(user=request.user,title=title,content=content)
         return redirect('entry_list')
     return render(request,'create_entry.html')
+
+@login_required
+def edit_entry(request,id):
+    entry=get_object_or_404(journalEntry,id=id,user=request.user)
+    if request.method=="POST":
+        entry.title=request.POST.get('title')
+        entry.content=request.POST.get('content')
+        entry.save()
+        return redirect('entry_list')
+    return render(request,'edit_entry.html',{'entry': entry})
+
+@login_required
+def delete_entry(request, id):
+
+    entry=get_object_or_404(journalEntry,id=id,user=request.user)
+    if request.method=="POST":
+        
+        entry.delete()
+        return redirect('entry_list')
+    return render(request,'delete_entry.html',{'entry': entry})
+
 
