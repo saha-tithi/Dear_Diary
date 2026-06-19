52ea31852ffe1bcaa6a59dabbeb82f8123b94037
saha-tithi <email.tithisaha@gmail.com>
2026-06-19 15:10:13 +0530

    Updated the UI

--- journal/models.py
+++ journal/models.py
@@ -2,11 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class journalEntry(models.Model):
+    ''' MOOD_CHOICES = [
+        ('happy', '😊 Happy'),
+        ('neutral', '😐 Neutral'),
+        ('sad', '😞 Sad'),
+        ('angry','😤angry'),
+    ]'''
+
+
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     title=models.CharField(max_length=200)
     content=models.TextField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
+    # mood=models.CharField(max_length=10,choices=MOOD_CHOICES,default='neutral')
         
     def __str__(self):
             return self.title
+
+   
+    
+   
